3b21aa4ef59dba54e2d611e4f652198c14736823
Nataliia <dominok56@gmail.com>
2022-11-11 16:23:53 +0200

    added task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
-# TODO: add initial code
-def hello_world():
-    return "Hello, world!"
+from abc import ABC
+
+
+class IntegerRange:
+    pass
+
+
+class Visitor:
+   pass
+
+
+class SlideLimitationValidator(ABC):
+    pass
+
+
+class ChildrenSlideLimitationValidator(SlideLimitationValidator):
+    pass
+
+
+class AdultSlideLimitationValidator(SlideLimitationValidator):
+    pass
+
+
+class Slide:
+    pass
